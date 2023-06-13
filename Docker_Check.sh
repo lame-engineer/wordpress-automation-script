d462965c2b110ecae7e0dcc148240cdf7e86f650
lame-engineer <88873676+lame-engineer@users.noreply.github.com>
2023-06-13 22:18:25 +0530

    Update Docker_Check.sh

--- Docker_Check.sh
+++ Docker_Check.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if command -v docker >/dev/null 2>&1;
+if command -v docker >/dev/null 2>&1;  #with this you can also have the location where the docker is actually residing
    command -v docker-compose >/dev/null 2>&1; then
+   # or you can use simply docker -v, through docker -v makes more sense.
     echo "Docker and Docker-Compose are installed."
-    # Additional actions for when Docker is installed
+    
 else
     echo "Docker is not installed. Installiation in progress..."
     sudo pacman -S docker docker-compose
-    # Additional actions for when Docker is not installed
+   
 fi
